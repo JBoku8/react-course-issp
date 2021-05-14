dfb89c7fe84f041e8ca2b135f1aa172ec8e8fe90
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-05-14 12:29:41 +0400

    provider children forms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-// import Home from "./pages/home";
-// import Users from "./pages/users";
-// import CounterHooks from "./pages/counter-hooks";
-// import CounterClass from "./pages/counter-class";
-import Expanses from "./pages/expanses";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/home';
+import Users from './pages/users';
+import CounterHooks from './pages/counter-hooks';
+import CounterClass from './pages/counter-class';
+import Books from './pages/books';
+import Expanses from './pages/expanses';
+import Theme from './components/theme/theme';
+import Navigation from './components/navigation/navigation';
+import BookProvider from './providers/BookProvider';
+import ExpanseProvider from './providers/ExpanseProvider';
 
-import "./App.css";
+import './App.css';
 
 // class = className
 // for = htmlFor
@@ -12,11 +18,41 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      {/* <Home /> */}
-      {/* <Users /> */}
-      {/* <CounterHooks initialValue={0} /> */}
-      {/* <CounterClass initialValue={0} /> */}
-      <Expanses />
+      <Navigation />
+      <hr />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/users">
+          <Users />
+        </Route>
+
+        <Route path="/counter-hooks">
+          <CounterHooks initialValue={0} />
+        </Route>
+
+        <Route path="/counter-class">
+          <CounterClass initialValue={0} />
+        </Route>
+
+        <Route path="/expanses">
+          <Theme>
+            <ExpanseProvider>
+              <Expanses />
+            </ExpanseProvider>
+          </Theme>
+        </Route>
+
+        <Route path="/books">
+          <BookProvider>
+            <Books />
+          </BookProvider>
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
--- src/pages/expanses/Expanses.js
+++ src/pages/expanses/Expanses.js
@@ -1,73 +1,24 @@
-import { useState } from 'react';
-
-// import { ExpansesFilter } from '../../components/filters/ExpansesFilter';
 import { ExpansesList } from '../../components/list';
 import ExpansesForm from '../../components/forms/expanses-form';
-import { expansesContext } from '../../contexts/expansesContext';
-import { expansesList } from '../../data';
-import { SORT_TYPE } from '../../utils';
 
 function Expanses() {
-  const [expansesData, setExpansesData] = useState(expansesList);
-  const [selectedExpanse, setSelectedExpanse] = useState(null);
-
-  const onExpanseSubmit = (data) => {
-    if (selectedExpanse) {
-      // update
-      const index = expansesData.findIndex((item) => item.id === selectedExpanse.id);
-      const leftData = expansesData.slice(0, index);
-      const updatedItem = {
-        ...expansesData[index],
-        ...data,
-      };
-      const rightData = expansesData.slice(index + 1);
-
-      setExpansesData([...leftData, updatedItem, ...rightData]);
-
-      setSelectedExpanse(null);
-    } else {
-      setExpansesData([...expansesData, data]);
-    }
-  };
-
-  const onSortHandler = (sortBy) => {
-    let sorted = [...expansesData];
-    if (sortBy === SORT_TYPE.ASC) {
-      sorted = sorted.sort((a, b) => a.amount - b.amount);
-    } else if (sortBy === SORT_TYPE.DESC) {
-      sorted = sorted.sort((a, b) => b.amount - a.amount);
-    }
-
-    setExpansesData(sorted);
-  };
-
   return (
-    <expansesContext.Provider
-      value={{
-        onExpanseSubmit,
-        selectedExpanse,
-        setSelectedExpanse,
-        onSortHandler,
-        expansesData,
-      }}>
-      <div className="expanses container">
-        <div className="row mt-5">
-          <h2 className="col-4 mx-auto text-muted text-center">ჩემი ხარჯები</h2>
-          <hr />
-        </div>
-        <div className="row mt-3">
-          <div className="col-6 mx-auto">
-            <ExpansesForm />
-          </div>
-        </div>
-        <div className="row mt-3">
-          {/* <ExpansesFilter /> */}
-          <div className="col-8 mx-auto mb-5">
-            <ExpansesList />
-          </div>
+    <div className="expanses container">
+      <div className="row mt-5">
+        <h2 className="col-4 mx-auto text-muted text-center">ჩემი ხარჯები</h2>
+        <hr />
+      </div>
+      <div className="row mt-3">
+        <div className="col-6 mx-auto">
+          <ExpansesForm />
         </div>
       </div>
-    </expansesContext.Provider>
+      <div className="row mt-3">
+        <div className="col-8 mx-auto mb-5">
+          <ExpansesList />
+        </div>
+      </div>
+    </div>
   );
 }
 
