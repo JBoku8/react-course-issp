97c19cf477686caf9c6b70fd7d5d5a31cd349370
Jano Bokuchava <jano.bokuchava@btu.edu.ge>
2021-06-29 11:55:20 +0400

    route parameter (#17)

--- src/components/cards/BookCard.js
+++ src/components/cards/BookCard.js
@@ -1,15 +1,23 @@
+import { Link } from 'react-router-dom'
+import { BOOKS_PATH } from '../../paths'
+
 import { bookCardIds } from '../../utils/testids'
 import css from './bookCard.module.css'
 
 export const BookCard = ({ item }) => {
   return (
     <div className="card border-0 col-4 book-item mb-2 p-3">
-      <img
-        className="card-img-top img-fluid h-50"
-        src={item.image}
-        alt={item.title}
-        data-testid={bookCardIds.image}
-      />
+      <Link
+        to={{
+          pathname: `${BOOKS_PATH}/${item.isbn}`,
+        }}>
+        <img
+          className="card-img-top img-fluid h-50"
+          src={item.image}
+          alt={item.title}
+          data-testid={bookCardIds.image}
+        />
+      </Link>
       <h2 className={`card-title ${css.title}`} data-testid={bookCardIds.title}>
         {item.title}
       </h2>
--- src/pages/books/Books.js
+++ src/pages/books/Books.js
@@ -1,19 +1,42 @@
-import { useContext, useEffect, useState } from 'react'
+import { memo, useContext, useEffect, useState, useCallback } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 import { BookCard } from '../../components/cards/BookCard'
 import AddBookForm from '../../components/forms/add-book-form/AddBookForm'
 import Spinner from '../../components/spinner/Spinner'
 import { booksContext, useBooks } from '../../providers/BookProvider'
+import { BOOKS_PATH } from '../../paths'
 
 import css from './books.module.css'
 
-function Books ({ initialAmount }) {
+const useQuery = (query) => {
+  return new URLSearchParams(query)
+}
+
+function Books({ initialAmount }) {
   const { loading, setAmount } = useContext(booksContext)
   const books = useBooks()
-  const [showForm, setShowForm] = useState(false)
+  const query = useQuery(useLocation().search)
+  const [showForm, setShowForm] = useState(() => {
+    return query.get('showForm') === 'true'
+  })
+  const history = useHistory()
+
+  const updateHistory = useCallback(() => {
+    history.push({
+      pathname: BOOKS_PATH,
+      search: '?' + query.toString(),
+    })
+  }, [query])
 
   useEffect(() => {
     setAmount(initialAmount)
   }, [initialAmount, setAmount])
+
+  useEffect(() => {
+    query.set('showForm', showForm)
+
+    updateHistory()
+  }, [showForm])
 
   const renderBooks = () => {
     if (loading) {
@@ -29,21 +52,34 @@ function Books ({ initialAmount }) {
     )
   }
 
+  const onShowHandler = () => {
+    const check = !(query.get('showForm') === 'true')
+    setShowForm(check)
+    query.set('showForm', check)
+    updateHistory()
+  }
+
+  const onFilterChange = (amount) => {
+    setAmount(amount)
+    query.set('amount', amount)
+    updateHistory()
+  }
+
   return (
     <div className="row">
       <div className="col-12">
         <h2 className={`text-muted ${css.title}`}>Books</h2>
         <div className="btn-group">
-          <button className="btn btn-primary" onClick={() => setShowForm(!showForm)}>
+          <button className="btn btn-primary" onClick={onShowHandler}>
             {showForm ? 'Hide' : 'Show'} Form
           </button>
-          <button className="btn btn-primary" onClick={() => setAmount(12)}>
+          <button className="btn btn-primary" onClick={() => onFilterChange(12)}>
             12
           </button>
-          <button className="btn btn-primary" onClick={() => setAmount(24)}>
+          <button className="btn btn-primary" onClick={() => onFilterChange(24)}>
             24
           </button>
-          <button className="btn btn-primary" onClick={() => setAmount(36)}>
+          <button className="btn btn-primary" onClick={() => onFilterChange(36)}>
             36
           </button>
         </div>
@@ -56,4 +92,4 @@ function Books ({ initialAmount }) {
   )
 }
 
-export default Books
+export default memo(Books)
